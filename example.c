ac714b2d211aa3592a494102ac53fb5e57cfc3f2
Hanno Rein <hanno@hanno-rein.de>
2021-05-04 19:17:11 -0400

    Second try. Better.

--- example.c
+++ example.c
@@ -5,72 +5,88 @@
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #define min(a,b) (((a) < (b)) ? (a) : (b))
 
-struct kb_test {
-    long N;
-    double* M;
-    double* e;
-    double* E;
-    double* E_solution;
-};
+//************************************
+// Dummy Kepler solver
+double kb_solver_simple(const double M, const double e, void* precalc){
+    double precalculated_value = *(double*)precalc;
+    // Return estimate for E
+    return M * e * precalculated_value;
+}
+
+// Precalculate values
+// Pointer can point to array, struct, etc
+void* kb_solver_simple_alloc(const double e){
+    double* p = malloc(sizeof(double));
+    *p = sqrt(1.);
+    return p;
+}
+
+// Free precalculated values
+void kb_solver_simple_free(void* p){
+    free(p);
+}
 
 struct kb_test_result {
-    int pass;
     double runtime;
     double error_max;
     double error_mean;
 };
 
-struct kb_test kb_test_simple_new(long N){
+//************************************
+// Dummy Kepler test case
+struct kb_test_result kb_test_simple(long N, double (*solver)(const double, const double, void* const), void* (*solver_alloc)(const double), void (*solver_free)(void* precalc)){
     // Allocate memory
-    struct kb_test test;
-    test.N = N;
-    test.M = malloc(sizeof(double)*N);
-    test.e = malloc(sizeof(double)*N);
-    test.E = malloc(sizeof(double)*N);
-    test.E_solution = malloc(sizeof(double)*N);
+    double* M = malloc(sizeof(double)*N);
+    double* E = malloc(sizeof(double)*N);
+    double* E_solution = malloc(sizeof(double)*N);
     // Initialize test
     for (long i=0; i<N; i++){
-        test.M[i] = i;
-        test.e[i] = i;
-        test.E_solution[i] = i;
+        M[i] = i;
+        E_solution[i] = i;
     }
-    return test;
-}
-
-void kb_test_simple_free(struct kb_test test){
-    free(test.M);
-    free(test.e);
-    free(test.E);
-    free(test.E_solution);
-}
-
-void kb_solver_simple(const double M, const double e, double* const E){
-    *E = M*e;
-}
-
-struct kb_test_result kb_test_run(struct kb_test const test, void (*solver)(const double, const double, double* const)){
+   
     struct timeval start, end;
-    gettimeofday(&start, NULL);
-    for (long i=0; i<test.N; i++){
-        solver(test.M[i], test.e[i], &(test.E[i]));
-    }
-    gettimeofday(&end, NULL);
     struct kb_test_result result = {0};
-    result.runtime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
-    for (long i=0; i<test.N; i++){
-        double error = fabs(test.E[i]-test.E_solution[i]);
-        result.error_max = max(result.error_max, error);
-        result.error_mean += error/test.N;
+    // Test different eccentricities, might vary between tests
+    for (double e=0.; e<1; e+=0.001){
+        // Run solver setup
+        void* precalc = NULL; // precalc can be zero
+        if (solver_alloc){
+            precalc = solver_alloc(e);
+        }
+        // Run test 
+        gettimeofday(&start, NULL);
+        for (long i=0; i<N; i++){
+            E[i] = solver(M[i], e, precalc);
+        }
+        if (solver_free){
+            solver_free(precalc);
+        }
+        gettimeofday(&end, NULL);
+        result.runtime += (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
     }
+
+    // Analyze results
+    for (long i=0; i<N; i++){
+        double error = fabs(E[i]-E_solution[i]);
+        result.error_max = max(result.error_max, error);
+        result.error_mean += error;
+    }
+    result.error_mean /= N;
+
+    // Free memory
+    free(M);
+    free(E);
+    free(E_solution);
     return result;
 }
 
+//////////////////////////////////////////////////////
 int main(int argc, char* argv[]){
-    long N = 1000000;
-    struct kb_test test = kb_test_simple_new(N);
-    struct kb_test_result result = kb_test_run(test, kb_solver_simple);
+    struct kb_test_result result = kb_test_simple(100000, kb_solver_simple, kb_solver_simple_alloc, kb_solver_simple_free);
+    // Solvers which do not precalculate anything can be called like this
+    // struct kb_test_result result = kb_test_simple(100000, kb_solver_simple, NULL, NULL);
     printf("runtime:    %.9fs\n", result.runtime); 
-    printf("error_max:  %.3e\n", result.error_max); 
-    printf("error_mean: %.3e\n", result.error_mean); 
-    kb_test_simple_free(test);
+    printf("error_max:  %.3e\n",  result.error_max); 
+    printf("error_mean: %.3e\n",  result.error_mean); 
 }
