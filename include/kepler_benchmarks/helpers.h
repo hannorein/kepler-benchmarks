248f2d105506f27e4351967c51a1f36c9614a9e4
Dan F-M <foreman.mackey@gmail.com>
2021-05-07 16:35:26 -0400

    adding mod function

--- include/kepler_benchmarks/helpers.h
+++ include/kepler_benchmarks/helpers.h
@@ -8,4 +8,23 @@
 
 inline int sign(double x) { return (x > 0) - (x < 0); }
 
+double mod_2pi(const double x) {
+  const double twopi = 2 * M_PI;
+  if (x < twopi && x >= 0) return x;
+
+  if (x > twopi) {
+    double M = x - twopi;
+    if (M > twopi)
+      return fmod(M, twopi);
+    else
+      return M;
+  } else {
+    double M = x + twopi;
+    if (M < 0)
+      return fmod(M, twopi) + twopi;
+    else
+      return M;
+  }
+}
+
 #endif
